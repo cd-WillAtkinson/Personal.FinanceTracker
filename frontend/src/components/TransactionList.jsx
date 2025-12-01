8c212787f2da9e62268a8aed10c5452b17276a0b
Will Atkinson <will.atkinson2023@my.ntu.ac.uk>
2025-12-01 23:53:31 +0000

    Creating blank canvas

---
+++ frontend/src/components/TransactionList.jsx
@@ -0,0 +1,10 @@
+export default function TransactionList() {
+    return (
+        <div>
+            <h2>Recent Transactions</h2>
+            <ul>
+                <li>No transactions yet.</li>
+            </ul>
+        </div>
+    );
+}
